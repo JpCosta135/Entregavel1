8ae931ff33fda313e9ae148807d22d59cd95d2fa
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2026-02-28 19:09:34 -0300

    Modify Principal class to include Primo instance
    Added a new instance of Primo and called ehPrimo method.

--- Nova pasta/entregavel 2 java/Principal.java
+++ Nova pasta/entregavel 2 java/Principal.java
@@ -1,5 +1,4 @@
 public class Principal {
-
     public static void main(String[] args) {
 
       AlgFund2 a2 = new AlgFund2();
@@ -8,6 +7,9 @@ public class Principal {
        AlgFund3 a3 = new AlgFund3()
         a3.soma();
 
+        Primo p1 = new Primo
+        p1.ehPrimo
+            
 int[] numeros = {10, 7, 8, 9, 1, 5};
 
         QuickSort.quicksort(numeros, 0, numeros.length - 1);
