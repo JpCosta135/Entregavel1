a2484c72615725ab3adb838bc14ef64b2f1c80b1
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2026-02-28 19:52:40 -0300

    Integrate user input for Fibonacci sequence generation
    Added user input for Fibonacci term generation and closed scanner.

--- Nova pasta/entregavel 2 java/Principal.java
+++ Nova pasta/entregavel 2 java/Principal.java
@@ -1,26 +1,28 @@
+import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
 
-      AlgFund2 a2 = new AlgFund2();
+        AlgFund2 a2 = new AlgFund2();
         a2.contagem();
-        
-       AlgFund3 a3 = new AlgFund3();
+        AlgFund3 a3 = new AlgFund3();
         a3.soma();
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Número de termos: ");
+        int n = sc.nextInt();
+        Fibonacci fib = new Fibonacci();
+        fib.gerar(n);
+        sc.close();
 
         Primo p1 = new Primo;
         p1.ehPrimo();
-            
         MDC m1 = new MDC;
         m1.mdc();    
             
-int[] numeros = {10, 7, 8, 9, 1, 5};
-
+        int[] numeros = {10, 7, 8, 9, 1, 5};
         QuickSort.quicksort(numeros, 0, numeros.length - 1);
-
         for (int num : numeros) {
             System.out.print(num + " ");
-       
+            
     }
-
 }
-
