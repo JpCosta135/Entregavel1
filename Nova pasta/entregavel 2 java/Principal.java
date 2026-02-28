c0b052e36a9f271f071ee2a6e8dff2b0e50dc019
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2026-02-28 16:34:09 -0300

    Implement QuickSort in Principal class
    Added QuickSort implementation to sort an array of numbers.

--- Nova pasta/entregavel 2 java/Principal.java
+++ Nova pasta/entregavel 2 java/Principal.java
@@ -2,14 +2,19 @@ public class Principal {
 
     public static void main(String[] args) {
 
-
       AlgFund2 a2 = new AlgFund2();
         a2.contagem();
 
        AlgFund3 a3 = new AlgFund3()
         a3.soma();
 
+int[] numeros = {10, 7, 8, 9, 1, 5};
 
+        QuickSort.quicksort(numeros, 0, numeros.length - 1);
+
+        for (int num : numeros) {
+            System.out.print(num + " ");
        
     }
+
 }
