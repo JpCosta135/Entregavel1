08d45d77eaa265813264405b9502226fb24134a1
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2026-04-04 15:57:05 -0300

    Implement user input for GCD calculation
    Added user input and output for GCD calculation.

--- Nova pasta/entregavel 1 js/mdc.js
+++ Nova pasta/entregavel 1 js/mdc.js
@@ -1,3 +1,8 @@
+let num1 = parseInt(prompt("Digite o primeiro número:"));
+let num2 = parseInt(prompt("Digite o segundo número:"));
+let resultado = mdc(num1, num2);
+document.write("O máximo divisor comum de " + num1 + " e " + num2 + " é: " + resultado);  
+
 function mdc(a, b) {
   while (b !== 0) {
     let resto = a % b;
