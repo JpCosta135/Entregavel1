164f4d5cb7c3241d47a13dd24eb6ef3d45d76d38
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2026-04-04 18:58:48 -0300

    update main class

--- Nova pasta/entregavel 2 java/Principal.java
+++ Nova pasta/entregavel 2 java/Principal.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
         Contagem a2 = new Contagem();
         a2.contagem();
-        AlgFund3 a3 = new AlgFund3();
+        Soma a3 = new Soma();
         a3.soma();
-        
-        Scanner sc = new Scanner(System.in);
+        Primo p1 = new Primo();
+        p1.ehPrimo();
+        MDC m1 = new MDC();
+        m1.mdc();
         System.out.print("Número de termos: ");
-        int n = sc.nextInt();
+        int n = input.nextInt();
         Fibonacci fib = new Fibonacci();
         fib.gerar(n);
-        sc.close();
 
-        Primo p1 = new Primo;
-        p1.ehPrimo();
-        MDC m1 = new MDC;
-        m1.mdc();    
-            
         int[] numeros = {10, 7, 8, 9, 1, 5};
         QuickSort.quicksort(numeros, 0, numeros.length - 1);
         for (int num : numeros) {
             System.out.print(num + " ");
-            
+
+        }
+
+
+         input.close();
+
     }
 }
